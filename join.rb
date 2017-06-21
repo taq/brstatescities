8e75ef466bd2d41905fc7602106b87534cc3bdc7
Eustaquio Rangel <taq@eustaquiorangel.com>
2017-06-21 17:10:49 -0300

    Simplified

--- join.rb
+++
@@ -1,8 +0,0 @@
-require "yaml"
-
-states = YAML.load(File.read("states.yml")).each {|key,val| val["cities"] = []}
-cities = YAML.load(File.read("cities.yml")).each {|key,val| states[val["state"]]["cities"] << val["name"]}
-
-for key, val in states
-   puts "#{val['name']} (#{key}) com #{val['cities'].size} cidades, sendo a primeira #{val['cities'].first}"
-end
